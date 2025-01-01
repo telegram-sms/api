425c9738d7dc30ccd21fd19df64ed54948213033
qwe7002-dev <qwe7002.dev@gmail.com>
2025-01-01 19:38:14 +0800

    Implementation allows cross-domain requests

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,46 @@
-import { AutoRouter } from 'itty-router';
+import { AutoRouter, cors } from 'itty-router';
 import Snowflake from './snowflake';
 
-const router = AutoRouter();
+// get preflight and corsify pair
+const { preflight, corsify } = cors({
+	origin: 'https://telegram-sms.com',
+  allowMethods: ['GET', 'PUT'],
+  allowHeaders: ['Content-Type']
+});
+
+const router = AutoRouter({
+  before: [preflight],  // add preflight upstream
+  finally: [corsify],   // and corsify downstream
+});
 
 router.get('/', () => {
-	return new Response('Hello Workers', {
-		status: 200
-	});
+  return new Response('Hello Workers', {
+    status: 200
+  });
 });
 
-router.put('/config', async (request, env) => {
-	const config: any = await request.json();
-	const encryptConfig = config.encrypt;
-	const snowflake = Snowflake.getInstance();
-	const key = snowflake.generateKey();
-	await env.telegram_config.put(key, encryptConfig, { expirationTtl: 3600 });
-	return new Response(JSON.stringify(
-		{ key: key }
-	), {
-		status: 20
-	});
+router.put('/config', async (request, env, context) => {
+  const config: any = await request.json();
+  const encryptConfig = config.encrypt;
+  const snowflake = Snowflake.getInstance();
+  const key = snowflake.generateKey();
+  await env.telegram_config.put(key, encryptConfig, { expirationTtl: 3600 });
+  return new Response(JSON.stringify(
+    { key: key }
+  ), {
+    status: 200
+  });
 });
 
-router.get('/config', async (request, env) => {
-	const config = await env.telegram_config.get(request.query['key']);
-	if (config === null) {
-		return new Response('Value not found', { status: 404 });
-	}
-	await env.telegram_config.delete(request.query['key']);
-	return new Response(config, {
-		status: 200
-	});
+router.get('/config', async (request, env, context) => {
+  const config = await env.telegram_config.get(request.query['key']);
+  if (config === null) {
+    return new Response('Value not found', { status: 404 });
+  }
+  await env.telegram_config.delete(request.query['key']);
+  return new Response(config, {
+    status: 200
+  });
 });
+
 export default { ...router };
